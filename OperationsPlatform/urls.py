7dd7709606432205054354adcdde9e80d3643c7f
yuanting <1967368321@qq.com>
2020-03-08 01:28:23 +0800

    add templates

--- OperationsPlatform/urls.py
+++ OperationsPlatform/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import operations_platform.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('lists/', operations_platform.views.lists),
 ]
--- operations_platform/views.py
+++ operations_platform/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Node
 
 # Create your views here.
+def lists(request):
+    # 从node节点中获取所有数据
+    data = Node.objects.all()
+
+    # 建立context字典，将值传递到页面
+    context = {
+        'data': data,
+    }
+
+    return render(request, 'lists.html', context)
