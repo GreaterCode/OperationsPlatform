a1fa862995d525219268304442add17ac1efeabc
yuanting <1967368321@qq.com>
2020-03-07 10:11:54 +0800

    Initial commit

---
+++ OperationsPlatform/__init__.py
@@ -0,0 +1,5 @@
+import pymysql
+pymysql.install_as_MySQLdb()
+
+
+## 后台用户/密码：yuanting/renwoxing
---
+++ operations_platform/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OperationsPlatformConfig(AppConfig):
+    name = 'operations_platform'
